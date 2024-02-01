26b97988574f913874f3e66dd0e7c773a5cba5ee
pepepondo2 <pepepondo2@gmail.com>
2024-02-01 19:53:15 +0800

    Added dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,34 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import tailwind from "@astrojs/tailwind";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: `Pondo's Super Secret Internal KB`,
-      social: {
-        github: 'https://github.com/withastro/starlight',
-      },
-      sidebar: [
-        {
-          label: 'Guides',
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', link: '/guides/example/' },
-          ],
-        },
-        {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' },
-        },
-        {
-          label: 'Knowledge Base',
-          autogenerate: { directory: 'knowledge' },
-        },
-      ],
-    }),
-  ],
+  integrations: [starlight({
+    title: `Pondo's Super Secret Internal KB`,
+    social: {
+      github: 'https://github.com/withastro/starlight'
+    },
+    sidebar: [{
+      label: 'Guides',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Example Guide',
+        link: '/guides/example/'
+      }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }, {
+      label: 'Knowledge Base',
+      autogenerate: {
+        directory: 'knowledge'
+      }
+    }]
+  }), tailwind(), vue()]
 });
